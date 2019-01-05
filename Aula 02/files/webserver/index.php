ab8cd5ecd5be1085176c4677141114ae87c65446
Diego Salmoria <diegorafael@gmail.com>
2019-01-05 15:57:16 -0200

    Adicionando os Playbooks da Aula 01 e 02.

---
+++ Aula 02/files/webserver/index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<html>
+<body>
+<center><font size="10" Color="size"> <br><br><br><br><br>
+O endereço IP deste Cliente é:
+<?php
+$ip=$_SERVER["REMOTE_ADDR"];
+echo $ip;
+?></font>
+</center>
+</body>
+</html>
